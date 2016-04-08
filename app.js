e37cf6f1dac4ad404f7447800216d566a2edcb6e
Marcus Nielsen <marcus.nielsen82@gmail.com>
2016-04-09 01:18:34 +0200

    .

---
+++ app.js
@@ -0,0 +1,23 @@
+var titleElm = document.querySelector('[data-title]')
+var titleAsArray = 'marcusnielsenorg'.split('')
+
+Rx.Observable.zip(
+Rx.Observable.fromArray(titleAsArray),
+Rx.Observable.interval(250),
+function (a, b) {
+  return a
+}  
+).scan(function(state, nextValue) {
+  return state + nextValue
+})
+.concat(
+  Rx.Observable.of('marcusnielsen org')
+  .delay(500)
+)
+.concat(
+  Rx.Observable.of('marcusnielsen_org')
+  .delay(500)
+)
+.subscribe(function(data) {
+  titleElm.text = data
+})
